331b61e4695afe3d8b6b4438820863bd3da28ddb
boqiu <82121246@qq.com>
2020-03-26 16:57:09 +0800

    add account to send tx with self managed nonce

---
+++ src/main/java/conflux/web3j/Account.java
@@ -0,0 +1,111 @@
+package conflux.web3j;
+
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.crypto.RawTransaction;
+
+public class Account {
+	
+	private Cfx cfx;
+	private AccountManager am;
+	private String address;
+	
+	private BigInteger gasPrice = CfxUnit.DEFAULT_GAS_PRICE;
+	private BigInteger nonce;
+	
+	public Account(Cfx cfx, AccountManager am, String address, String password) throws Exception {
+		this(cfx, am, address, password, null);
+	}
+	
+	public Account(Cfx cfx, AccountManager am, String address, String password, Duration unlockTimeout) throws Exception {
+		if (!am.unlock(address, password, unlockTimeout)) {
+			throw new Exception("account not found in keystore");
+		}
+		
+		this.cfx = cfx;
+		this.am = am;
+		this.address = address;
+		
+		this.nonce = cfx.getTransactionCount(address).sendAndGet();
+	}
+	
+	public BigInteger getNonce() {
+		return nonce;
+	}
+	
+	public BigInteger getGasPrice() {
+		return gasPrice;
+	}
+	
+	public Account withGasPrice(BigInteger gasPrice) {
+		this.gasPrice = gasPrice;
+		return this;
+	}
+	
+	public String send(RawTransaction tx) throws Exception {
+		String signedTx = this.am.signTransaction(tx, this.address);
+		String txHash = this.cfx.sendRawTransaction(signedTx).sendAndGet();
+		
+		if (txHash == null || txHash.isEmpty()) {
+			throw new Exception("failed to send transaction, tx hash is null or empty");
+		}
+		
+		this.nonce = this.nonce.add(BigInteger.ONE);
+		
+		return txHash;
+	}
+	
+	public String transfer(String to, BigInteger value) throws Exception {
+		RawTransaction tx = RawTransaction.createEtherTransaction(this.nonce, this.gasPrice, CfxUnit.DEFAULT_GAS_LIMIT, to, value);
+		return this.send(tx);
+	}
+	
+	public String deploy(BigInteger gasLimit, String bytecodes) throws Exception {
+		return this.deploy(gasLimit, BigInteger.ZERO, bytecodes);
+	}
+	
+	public String deploy(BigInteger gasLimit, BigInteger value, String bytecodes) throws Exception {
+		RawTransaction tx = RawTransaction.createContractTransaction(this.nonce, this.gasPrice, gasLimit, value, bytecodes);
+		return this.send(tx);
+	}
+	
+	public String call(String contract, BigInteger gasLimit, String method, Type<?>... inputs) throws Exception {
+		return this.call(contract, gasLimit, BigInteger.ZERO, method, inputs);
+	}
+	
+	public String call(String contract, BigInteger gasLimit, BigInteger value, String method, Type<?>... inputs) throws Exception {
+		String data = "";
+		
+		if (method != null && !method.isEmpty()) {
+			Function function = new Function(method, Arrays.asList(inputs), Collections.emptyList());
+			data = FunctionEncoder.encode(function);
+		}
+		
+		RawTransaction tx = RawTransaction.createTransaction(this.nonce, this.gasPrice, gasLimit, contract, value, data);
+		return this.send(tx);
+	}
+	
+	public String call(String contract, BigInteger gasLimit, String data) throws Exception {
+		return this.call(contract, gasLimit, BigInteger.ZERO, data);
+	}
+	
+	public String call(String contract, BigInteger gasLimit, BigInteger value, String data) throws Exception {
+		RawTransaction tx = RawTransaction.createTransaction(this.nonce, this.gasPrice, gasLimit, contract, value, data);
+		return this.send(tx);
+	}
+	
+	public void waitForNonceUpdated() throws InterruptedException {
+		this.cfx.waitForNonce(this.address, this.nonce);
+	}
+	
+	public void waitForNonceUpdated(long intervalMillis) throws InterruptedException {
+		this.cfx.waitForNonce(this.address, this.nonce, intervalMillis);
+	}
+
+}
--- src/main/java/conflux/web3j/contract/ERC20Executor.java
+++ src/main/java/conflux/web3j/contract/ERC20Executor.java
@@ -5,30 +5,28 @@ import java.math.BigInteger;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.generated.Uint256;
 
-import conflux.web3j.AccountManager;
-import conflux.web3j.Cfx;
-import conflux.web3j.RpcException;
+import conflux.web3j.Account;
 
-public class ERC20Executor extends ContractExecutor {
+public class ERC20Executor {
 	
-	public ERC20Executor(Cfx cfx, AccountManager am, String userAddress, String erc20Address) throws RpcException {
-		super(cfx, am, userAddress, erc20Address);
-	}
+	private Account account;
+	private String contract;
 	
-	public ERC20Executor(Cfx cfx, AccountManager am, String userAddress, BigInteger nonce, String erc20Address) {
-		super(cfx, am, userAddress, nonce, erc20Address);
+	public ERC20Executor(Account account, String erc20Address) {
+		this.account = account;
+		this.contract = erc20Address;
 	}
 	
 	public String transfer(BigInteger gasLimit, String recipient, BigInteger amount) throws Exception {
-		return this.execute(gasLimit, "transfer", new Address(recipient), new Uint256(amount));
+		return this.account.call(this.contract, gasLimit, "transfer", new Address(recipient), new Uint256(amount));
 	}
 	
 	public String approve(BigInteger gasLimit, String spender, BigInteger amount) throws Exception {
-		return this.execute(gasLimit, "approve", new Address(spender), new Uint256(amount));
+		return this.account.call(this.contract, gasLimit, "approve", new Address(spender), new Uint256(amount));
 	}
 	
 	public String transferFrom(BigInteger gasLimit, String sender, String recipient, BigInteger amount) throws Exception {
-		return this.execute(gasLimit, "transferFrom", new Address(sender), new Address(recipient), new Uint256(amount));
+		return this.account.call(this.contract, gasLimit, "transferFrom", new Address(sender), new Address(recipient), new Uint256(amount));
 	}
 
 }
