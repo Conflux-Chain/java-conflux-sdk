f5b2fc2b0b9aa45e75201d60d498bc047f87280c
W <pana.wang@outlook.com>
2020-11-20 17:40:43 +0800

    receipt add field: txExecErrMsg (#13)
    * receipt add field
    
    * update readme add more doc
    
    * receipt add field
    
    * code rebase
    
    * add setter
    
    * sync
    
    * fix bug

--- src/main/java/conflux/web3j/response/Receipt.java
+++ src/main/java/conflux/web3j/response/Receipt.java
@@ -22,6 +22,7 @@ public class Receipt {
 	private String logsBloom;
 	private String stateRoot;
 	private String outcomeStatus;
+	private String txExecErrorMsg;
 	
 	public String getTransactionHash() {
 		return transactionHash;
@@ -126,4 +127,10 @@ public class Receipt {
 	public void setOutcomeStatus(String outcomeStatus) {
 		this.outcomeStatus = outcomeStatus;
 	}
+
+	public String getTxExecErrorMsg() {
+		return this.txExecErrorMsg;
+	}
+
+	public void setTxExecErrorMsg(String errorMsg) {this.txExecErrorMsg = errorMsg;}
 }
