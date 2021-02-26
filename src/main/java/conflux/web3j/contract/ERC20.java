948e8e78209f78b7acf3514791ca0651740adda5
W <pana.wang@outlook.com>
2021-02-26 10:38:57 +0800

    When interact with contract method use AbiAddress (#22)
    * When interact with contract method use AbiAddress
    
    * add new add CfxAddress
    
    Co-authored-by: PanaW <wangpan@conflux-chain.org>

--- src/main/java/conflux/web3j/contract/ERC20.java
+++ src/main/java/conflux/web3j/contract/ERC20.java
@@ -3,21 +3,22 @@ package conflux.web3j.contract;
 import java.math.BigInteger;
 
 import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.Address;
 
 import conflux.web3j.Account;
-import conflux.web3j.types.Address;
 import conflux.web3j.Cfx;
 import conflux.web3j.RpcException;
+import conflux.web3j.types.CfxAddress;
 
 public class ERC20 extends ContractCall{
     private Account account;
-    private Address contract;
+    private CfxAddress contract;
 
-    public ERC20(Cfx cfx, Address address) {
+    public ERC20(Cfx cfx, CfxAddress address) {
         super(cfx, address);
     }
 
-    public ERC20(Cfx cfx, Address address, Account account) {
+    public ERC20(Cfx cfx, CfxAddress address, Account account) {
         super(cfx, address);
         this.account = account;
         this.contract = address;
@@ -28,22 +29,22 @@ public class ERC20 extends ContractCall{
     }
 
     public BigInteger balanceOf(Address account) throws RpcException {
-        return this.callAndGet(Uint256.class, "balanceOf", account.getABIAddress());
+        return this.callAndGet(Uint256.class, "balanceOf", account);
     }
 
     public BigInteger allowance(Address owner, Address spender) throws RpcException {
-        return this.callAndGet(Uint256. class, "allowance", owner.getABIAddress(), spender.getABIAddress());
+        return this.callAndGet(Uint256. class, "allowance", owner, spender);
     }
 
     public String transfer(Account.Option option, Address recipient, BigInteger amount) throws Exception {
-        return this.account.call(option, this.contract, "transfer", recipient.getABIAddress(), new Uint256(amount));
+        return this.account.call(option, this.contract, "transfer", recipient, new Uint256(amount));
     }
 
     public String approve(Account.Option option, Address spender, BigInteger amount) throws Exception {
-        return this.account.call(option, this.contract, "approve", spender.getABIAddress(), new Uint256(amount));
+        return this.account.call(option, this.contract, "approve", spender, new Uint256(amount));
     }
 
     public String transferFrom(Account.Option option, Address sender, Address recipient, BigInteger amount) throws Exception {
-        return this.account.call(option, this.contract, "transferFrom", sender.getABIAddress(), recipient.getABIAddress(), new Uint256(amount));
+        return this.account.call(option, this.contract, "transferFrom", sender, recipient, new Uint256(amount));
     }
 }
--- src/main/java/conflux/web3j/contract/internals/AdminControl.java
+++ src/main/java/conflux/web3j/contract/internals/AdminControl.java
@@ -1,49 +1,51 @@
 package conflux.web3j.contract.internals;
 
-import conflux.web3j.types.Address;
 import conflux.web3j.Account;
 import conflux.web3j.Account.Option;
 import conflux.web3j.Cfx;
 import conflux.web3j.RpcException;
 import conflux.web3j.contract.ContractCall;
+import org.web3j.abi.datatypes.Address;
+import conflux.web3j.types.CfxAddress;
 
 public class AdminControl extends ContractCall {
     private final static String contract = "0x0888000000000000000000000000000000000000";
     private Account account;  // if account not set, can only use getAdmin method
-    private Address contractAddress;
+    private CfxAddress contractAddress;
 
     public AdminControl(Account account, int networkId) {
-        super(account.getCfx(), new Address(AdminControl.contract, networkId));
+        super(account.getCfx(), new CfxAddress(AdminControl.contract, networkId));
+        this.contractAddress = new CfxAddress(AdminControl.contract, networkId);
         this.account = account;
-        this.contractAddress = new Address(AdminControl.contract, networkId);
     }
 
     public AdminControl(Cfx cfx) {
-        super(cfx, new Address(AdminControl.contract, cfx.getIntNetworkId()));
-        this.contractAddress = new Address(AdminControl.contract, cfx.getIntNetworkId());
+        super(cfx, new CfxAddress(AdminControl.contract, cfx.getIntNetworkId()));
+        this.contractAddress = new CfxAddress(AdminControl.contract, cfx.getIntNetworkId());
     }
 
     public void setAccount(Account account) {
         this.account = account;
     }
 
-    public String getAdmin(Address contractAddr) throws RpcException {
-        return this.callAndGet(org.web3j.abi.datatypes.Address.class, "getAdmin", contractAddr.getABIAddress());
+    public Address getAdmin(Address contractAddr) throws RpcException {
+        String hexAddress = this.callAndGet(Address.class, "getAdmin", contractAddr);
+        return new Address(hexAddress);
     }
 
     public String destroy (Option option, Address contractAddr) throws Exception {
-        String admin = getAdmin(contractAddr);
-        if (!admin.equalsIgnoreCase(account.getAddress().getHexAddress())) {
+        Address admin = getAdmin(contractAddr);
+        if (!admin.getValue().equalsIgnoreCase(account.getAddress().getHexAddress())) {
             throw new Exception("Administrator privilege required");
         }
-        return this.account.call(option, this.contractAddress, "destroy", contractAddr.getABIAddress());
+        return this.account.call(option, this.contractAddress, "destroy", contractAddr);
     }
 
     public String setAdmin(Option option, Address contractAddr, Address newAdmin) throws Exception {
-        String admin = getAdmin(contractAddr);
-        if (!admin.equalsIgnoreCase(account.getAddress().getHexAddress())) {
+        Address admin = getAdmin(contractAddr);
+        if (!admin.getValue().equalsIgnoreCase(account.getAddress().getHexAddress())) {
             throw new Exception("Administrator privilege required");
         }
-        return this.account.call(option, this.contractAddress, "setAdmin", contractAddr.getABIAddress(), newAdmin.getABIAddress());
+        return this.account.call(option, this.contractAddress, "setAdmin", contractAddr, newAdmin);
     }
 }
